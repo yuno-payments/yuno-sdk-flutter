a7343297f4fa16e724cd7ac49d2624ec29f9889e
yuno-steven <guerin.colocho@y.uno>
2024-07-30 15:27:38 -0600

    fix: rm cache for foundation package

--- yuno_sdk_foundation/lib/yuno_sdk_foundation.dart
+++
@@ -1,8 +0,0 @@
-
-import 'yuno_sdk_foundation_platform_interface.dart';
-
-class YunoSdkFoundation {
-  Future<String?> getPlatformVersion() {
-    return YunoSdkFoundationPlatform.instance.getPlatformVersion();
-  }
-}
