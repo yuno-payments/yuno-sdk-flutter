5bf97c3b35666c121d28adfab5994c27d31bcb22
yuno-steven <guerin.colocho@y.uno>
2024-08-16 10:10:05 -0600

    fix: ident and common errors

--- yuno_sdk_foundation/ios/Classes/YunoMethods/YunoMethods.swift
+++ yuno_sdk_foundation/ios/Classes/YunoMethods/YunoMethods.swift
@@ -13,10 +13,9 @@ class YunoMethods:  YunoPaymentDelegate, YunoMethodsViewDelegate {
     
     var countryCode: String = ""
     var checkoutSession: String = ""
-   
-   
+    
     func yunoPaymentResult(_ result: YunoSDK.Yuno.Result) {
-       
+        
         
     }
     
@@ -46,21 +45,21 @@ class YunoMethods:  YunoPaymentDelegate, YunoMethodsViewDelegate {
             config: YunoConfig(
                 cardFormType:cardFormType?.toCardFormType ?? .oneStep,
                 appearance: Yuno.Appearance(
-                accentColor: appearance?.accentColor,
-                buttonBackgroundColor:  appearance?.buttonBackgroundColor,
-                buttonTitleColor: appearance?.buttonTitleBackgroundColor,
-                buttonBorderColor:  appearance?.buttonBorderBackgroundColor,
-                secondaryButtonBackgroundColor: appearance?.secondaryButtonBackgroundColor,
-                secondaryButtonTitleColor: appearance?.secondaryButtonTitleBackgroundColor,
-                secondaryButtonBorderColor: appearance?.secondaryButtonBorderBackgroundColor,
-                disableButtonBackgroundColor:appearance?.disableButtonBackgroundColor,
-                disableButtonTitleColor:appearance?.disableButtonTitleBackgroundColor,
-                checkboxColor: appearance?.checkboxColor),
-            saveCardEnabled: configuration?.saveCardEnable ?? false,
-            keepLoader: configuration?.keepLoader ?? false,
-            isDynamicViewEnabled: configuration?.isDynamicViewEnable ?? false
+                    accentColor: appearance?.accentColor,
+                    buttonBackgroundColor:  appearance?.buttonBackgroundColor,
+                    buttonTitleColor: appearance?.buttonTitleBackgroundColor,
+                    buttonBorderColor:  appearance?.buttonBorderBackgroundColor,
+                    secondaryButtonBackgroundColor: appearance?.secondaryButtonBackgroundColor,
+                    secondaryButtonTitleColor: appearance?.secondaryButtonTitleBackgroundColor,
+                    secondaryButtonBorderColor: appearance?.secondaryButtonBorderBackgroundColor,
+                    disableButtonBackgroundColor:appearance?.disableButtonBackgroundColor,
+                    disableButtonTitleColor:appearance?.disableButtonTitleBackgroundColor,
+                    checkboxColor: appearance?.checkboxColor),
+                saveCardEnabled: configuration?.saveCardEnable ?? false,
+                keepLoader: configuration?.keepLoader ?? false,
+                isDynamicViewEnabled: configuration?.isDynamicViewEnable ?? false
             )
-        
+            
         )
         Yuno.startCheckout(with: self)
     }
@@ -70,13 +69,13 @@ class YunoMethods:  YunoPaymentDelegate, YunoMethodsViewDelegate {
 
 extension YunoMethods {
     
-     func handleInitialize(call: FlutterMethodCall, result: @escaping FlutterResult) {
+    func handleInitialize(call: FlutterMethodCall, result: @escaping FlutterResult) {
         
-        guard let args = call.arguments as? [String:Any] else{
+        guard let args = call.arguments as? [String:Any] else {
             return  result(FlutterError(code: "4", message: "Invalid arguments", details: "Arguments are invalid"))
         }
         
-        do{
+        do {
             let jsonData = try JSONSerialization.data(withJSONObject: args, options: [])
             let decoder = JSONDecoder()
             let app = try decoder.decode(AppConfiguration.self, from: jsonData)
@@ -86,12 +85,12 @@ extension YunoMethods {
             }
             
             self.initialize(app: app )
-     
+            
             return result(true)
             
-        }catch{
-         return   result(FlutterError(code: "0", message: "Unexpected Exception", details: "Something went wrong"))
+        } catch {
+            return result(FlutterError(code: "0", message: "Unexpected Exception", details: "Something went wrong"))
         }
-     
+        
     }
 }
