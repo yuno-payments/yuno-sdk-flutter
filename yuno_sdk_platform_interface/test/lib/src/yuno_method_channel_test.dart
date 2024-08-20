60c0e2f19f82ecac3e760bbc90a4e4b80892f0ee
yuno-steven <guerin.colocho@y.uno>
2024-08-20 09:07:09 -0600

    feat:add keeping test

--- yuno_sdk_platform_interface/test/lib/src/yuno_method_channel_test.dart
+++ yuno_sdk_platform_interface/test/lib/src/yuno_method_channel_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -133,5 +135,24 @@ void main() {
 
       expect(methodCalled, isTrue);
     });
+  });
+
+  test('Factory creates YunoMethodChannel with correct platform flags', () {
+    // Factory should set the correct platform flags
+    YunoMethodChannelFactory factory = const YunoMethodChannelFactory();
+
+    if (Platform.isIOS) {
+      YunoPlatform instance = factory.create();
+      expect(instance is YunoMethodChannel, true);
+      final yuno = instance as YunoMethodChannel;
+      expect(yuno.isIos, true);
+      expect(yuno.isAndroid, false);
+    } else if (Platform.isAndroid) {
+      YunoPlatform instance = factory.create();
+      expect(instance is YunoMethodChannel, true);
+      final yuno = instance as YunoMethodChannel;
+      expect(yuno.isIos, false);
+      expect(yuno.isAndroid, true);
+    }
   });
 }
