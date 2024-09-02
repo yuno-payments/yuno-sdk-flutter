d406af32168493e0f813fadc5ff9d36e910e75ba
yuno-steven <guerin.colocho@y.uno>
2024-09-02 10:03:39 -0600

    fix: update grammar

--- yuno_sdk_foundation/ios/Classes/YunoMethods/Models/AppConfiguration.swift
+++ yuno_sdk_foundation/ios/Classes/YunoMethods/Models/AppConfiguration.swift
@@ -66,7 +66,7 @@ struct Configuration: Codable, Sendable {
     let appearance: Appearance?
 }
 
-struct YunoConfiguration: Codable, Sendable{
+struct YunoConfiguration: Codable, Sendable {
     let lang: String
     let cardFlow: String?
     let saveCardEnable: Bool?
