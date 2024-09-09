d6877f3af2ba77c12f4eee48c92a6d59621435dc
yuno-steven <guerin.colocho@y.uno>
2024-09-09 10:48:04 -0600

    feat: add update checkoutSession

--- yuno_sdk_android/android/src/main/kotlin/com/example/yuno_sdk_android/features/start_payment_lite/method_channels/StartPaymentLiteHandler.kt
+++ yuno_sdk_android/android/src/main/kotlin/com/example/yuno_sdk_android/features/start_payment_lite/method_channels/StartPaymentLiteHandler.kt
@@ -3,6 +3,7 @@ package com.example.yuno_sdk_android.features.start_payment_lite.method_channels
 import android.content.Context
 import com.yuno.payments.features.payment.startPaymentLite
 import com.yuno.payments.features.payment.ui.views.PaymentSelected
+import com.yuno.payments.features.payment.updateCheckoutSession
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel.Result
@@ -12,6 +13,7 @@ class StartPaymentLiteHandler {
     fun handler(call: MethodCall, result: Result, context: Context, activity: FlutterFragmentActivity){
         try {
             //TODO: implement dynamic startPayment
+            activity.updateCheckoutSession()
             activity.startPaymentLite(
                 paymentSelected = PaymentSelected(
                     paymentMethodType = "",
