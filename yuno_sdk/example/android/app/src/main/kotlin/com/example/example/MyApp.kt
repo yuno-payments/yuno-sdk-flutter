381a8b4230dcecb542303401c022ed06ceb61940
yuno-steven <guerin.colocho@y.uno>
2024-10-09 08:34:56 -0600

    fix: space

--- yuno_sdk/example/android/app/src/main/kotlin/com/example/example/MyApp.kt
+++ yuno_sdk/example/android/app/src/main/kotlin/com/example/example/MyApp.kt
@@ -5,6 +5,6 @@ import com.yuno_flutter.yuno_sdk_android.YunoSdkAndroidPlugin
 class MyApp: Application() {
     override fun onCreate() {
         super.onCreate()
-       YunoSdkAndroidPlugin.initSdk(this, BuildConfig.YUNO_API_KEY)
+        YunoSdkAndroidPlugin.initSdk(this, BuildConfig.YUNO_API_KEY)
     }
 }
