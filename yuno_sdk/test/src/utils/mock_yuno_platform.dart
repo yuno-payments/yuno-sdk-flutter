8d8ce87d8180098dd3e236966c9ac18e4bba9574
yuno-steven <guerin.colocho@y.uno>
2024-11-01 08:40:39 -0600

    fix: upgrade uri

--- yuno_sdk/test/src/utils/mock_yuno_platform.dart
+++ yuno_sdk/test/src/utils/mock_yuno_platform.dart
@@ -1,6 +1,6 @@
 import 'package:mocktail/mocktail.dart';
 import 'package:plugin_platform_interface/plugin_platform_interface.dart';
-import 'package:yuno_sdk_platform_interface/lib.dart';
+import 'package:yuno_sdk_platform_interface/yuno_sdk_platform_interface.dart';
 
 class MockYunoPlatform extends Mock
     with MockPlatformInterfaceMixin
