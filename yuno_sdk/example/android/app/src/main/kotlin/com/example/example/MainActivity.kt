7bf4ffa8510ba88d97504901846c356fd037c596
yuno-steven <guerin.colocho@y.uno>
2024-11-15 14:06:06 -0600

    feat: release version

--- yuno_sdk/example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
+++ yuno_sdk/example/android/app/src/main/kotlin/com/example/example/MainActivity.kt
@@ -1,4 +1,5 @@
 package com.example.example
+
 import io.flutter.embedding.android.FlutterFragmentActivity
 
 class MainActivity: FlutterFragmentActivity()
--- yuno_sdk/example/lib/core/feature/utils/yuno_assets.dart
+++
@@ -1,4 +0,0 @@
-final class YunoAssets {
-  static String appleIC = 'assets/images/apple.png';
-  static String androidIC = 'assets/images/android.png';
-}
