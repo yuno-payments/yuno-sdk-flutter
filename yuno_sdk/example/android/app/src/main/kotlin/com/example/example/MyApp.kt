6f7cd0b30883669dee72d3a599f44bff0883ef38
yuno-steven <guerin.colocho@y.uno>
2024-11-15 14:44:41 -0600

    fix: remove hardcode api key

--- yuno_sdk/example/android/app/src/main/kotlin/com/example/example/MyApp.kt
+++ yuno_sdk/example/android/app/src/main/kotlin/com/example/example/MyApp.kt
@@ -5,6 +5,6 @@ import com.yuno_flutter.yuno_sdk_android.YunoSdkAndroidPlugin
 class MyApp : Application() {
     override fun onCreate() {
         super.onCreate()
-        YunoSdkAndroidPlugin.initSdk(this, "staging_gAAAAABmfZ48j4sIsCGFlFxY5aipm0osWo8wfbwdTnGMX2RY-BnLPlUPlLSEyarnWan3ymzFz0zgw9HXYejiBzT10ihhCdKW3GpCOWCE2NR1d85Q0R6_pZ4NyCzvkGgrJFC1XMH987EHx15OtTq4OqPDZnGl0go5bGwdD1i3g1doYs_n-DUS2R2vCcCIf5fYceCvC7PrQLfWuLK5kAP5OL4IQBIAp6NagOzj9JRlkyxT_6SoxIAmwsPgcF-KOn-gpiLdu_OIwUOX")
+        YunoSdkAndroidPlugin.initSdk(this, BuildConfig.YUNO_API_KEY)
     }
 }
