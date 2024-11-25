8cc83b00a85d3ef5e407b3ef0336f8278dd8445b
yuno-steven <guerin.colocho@y.uno>
2024-11-25 08:49:12 -0600

    feat:checked

--- yuno_sdk/test/src/widgets/yuno_reader_test.dart
+++ yuno_sdk/test/src/widgets/yuno_reader_test.dart
@@ -98,7 +98,7 @@ void main() {
       );
     });
 
-    testWidgets('continue payment is called with correct arguments',
+    testWidgets('Continue payment is called with correct arguments',
         (WidgetTester tester) async {
       await tester.pumpWidget(
         Builder(
